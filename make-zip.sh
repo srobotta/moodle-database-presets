615d6af71148520ee57322c9e1c0c443db4e5c1b
Stephan Robotta <stephan.robotta@bfh.ch>
2023-11-21 11:02:39 +0100

    update the zip creation process

--- make-zip.sh
+++ make-zip.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 # Create a zip from the directories with the database
 # templates and preset.xml for direct upload in the
-# database activity.
+# database activity. Traverse the current directories
+# and check for an preset.xml file. If this is present,
+# then assume that we have a directory with templates,
+# therefore zip the relevant files and place the zip archive
+# in the root directory.
 #
-# Usage: bash make-zip-sh <directory>
-# Result: a <directory>.zip in the root directory. 
+# Usage: bash make-zip.sh
+# Result: a <directory>.zip in the root directory for
+# every preset.
 
-if [ -z $1 ]; then
-  echo 'missing directory argument'
-  exit 1
-fi
+directories=$(ls -p | grep -e /$)
 
-if [ ! -d $1 ]; then
-  echo 'directory does not exist'
-  exit 2
-fi
+for dir in $directories
+do
+  if [ ! -e ${dir}preset.xml ]; then
+    continue
+  fi
 
-zipfile="${1}.zip"
-if [ -e "$zipfile" ]; then
-  rm $zipfile
-fi
+  zipfile="${dir:0:-1}.zip"
+  if [ -e "$zipfile" ]; then
+    rm $zipfile
+  fi
 
-cd $1
-zip $zipfile  \
-  addtemplate.html \
-  jstemplate.js \
-  listtemplateheader.html \
-  preset.xml \
-  rsstitletemplate.html \
-  asearchtemplate.html \
-  csstemplate.css \
-  listtemplatefooter.html \
-  listtemplate.html \
-  rsstemplate.html \
-  singletemplate.html
+  cd $dir
+  zip $zipfile  \
+    addtemplate.html \
+    jstemplate.js \
+    listtemplateheader.html \
+    preset.xml \
+    rsstitletemplate.html \
+    asearchtemplate.html \
+    csstemplate.css \
+    listtemplatefooter.html \
+    listtemplate.html \
+    rsstemplate.html \
+    singletemplate.html
 
-cd ..
-mv $1/$zipfile .
+  cd ..
+  mv ${dir}${zipfile} .
 
+done
